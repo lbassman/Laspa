77d3580ddf6e6e9172cddaf58ca8a1de6e1398ad
jonaskaufman <Jonas@Jonas-MBP-2.materials.unsw.edu.au>
2015-06-22 17:33:11 +1000

    bug fix in writing to POSCARs

--- Cell.py
+++ Cell.py
@@ -297,7 +297,7 @@ class Cell:
         string += (' %d'*len(self.elementCounts))%tuple(self.elementCounts) + '\n'
         if self.SelectiveDynamics:
             string += 'Selective Dynamics\n'
-        string += self.CorD
+        string += self.CorD+'\n'
         # add atom sites
         for element in self.sites:
             for k in element:
