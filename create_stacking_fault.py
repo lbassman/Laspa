133a7237f6bf5cec9bfc019966bbf13f62879ab9
jonaskaufman <Jonas@Jonas-MBP-2.materials.unsw.edu.au>
2015-06-26 15:12:51 +1000

    added ESF

--- create_stacking_fault.py
+++ create_stacking_fault.py
@@ -1,15 +1,10 @@
 # Jonas Kaufman jlkaufman@gmail.com
 # June 22 2014
-# Script to create POSCARs for several points along the (111)
-# generalized stacking fault pathway in an fcc crystal
-# and submit all jobs to the queue on Stampede
 
+from pylab import *
 from Cell import *
 import math
 
-#===========================================================================
-# MAIN PROGRAM
-#===========================================================================
 def addA(cell,z):
     cell.addSite(0,Site([0,0,z]))
     cell.addSite(0,Site([0.5,0.5,z]))
@@ -20,28 +15,29 @@ def addC(cell,z):
     cell.addSite(0,Site([0,(4.0/6.0),z]))
     cell.addSite(0,Site([0.5,(1.0/6.0),z]))
 
+# add ESF and ISF functions
+
 #===========================================================================
 # MAIN PROGRAM
 #===========================================================================
+"""
 element = raw_input('Element: ')
 a = float(raw_input('Lattice parameter: '))
 faultType = raw_input('Stacking fault type (P/I/E): ')
 n = int(raw_input('Number of 3-layer stacks on either side of fault: '))
+"""
+element = 'Cu'
+a = 3.63548
+ngap = 3
 
-sequence = n*'ABC'
-if faultType == 'I':
-    sequence += 'BC'
-elif faultType == 'E':
-    sequence += 'ACBC'
-sequence += n*'ABC'
+sequence = 'ABCABCABCABC'
 layers = list(sequence)
 nlayers = len(layers)
-print sequence
 
 ax = math.sqrt(2)/2
 ay = math.sqrt(6)/2
 az = math.sqrt(3)/3
-bx,by,bz = ax,ay,az*nlayers
+bx,by,bz = ax,ay,az*(nlayers+ngap)
 
 cell = Cell() # Cartesian coordinates by default
 cell.setHeader(element+' fcc')
@@ -50,7 +46,7 @@ cell.setA0(a)
 cell.setCoordinateSystem('Direct')
 cell.setLatticeVectors([[bx,0,0],[0,by,0],[0,0,bz]])
 
-zpoint = 0.0
+zpoint = 0
 while layers:
     next = layers.pop(0)
     if next == 'A':
@@ -59,7 +55,40 @@ while layers:
         addB(cell,zpoint)
     else: # next == 'C'
         addC(cell,zpoint)
-    zpoint += (1.0/nlayers)
+    zpoint += (1.0/(nlayers+ngap))
+
+halfz = .5*(nlayers-1)/(nlayers+ngap) # z position right between the surface layers
 
 cell.setSiteMobilities(False,False,True)
 cell.sendToPOSCAR()
+
+n_points = 5
+displacements = np.linspace(0.0, 1.0, n_points)
+
+# ISF
+for disp in displacements:    
+    cell = Cell().loadFromPOSCAR()
+    for element in cell.sites:
+        for i in range(cell.numberOfElements()):
+            for j in range(cell.numberOfAtomsOfElement(i)):
+                    x,y,z = cell.sites[i][j].position
+                    if z > halfz:
+                        cell.sites[i][j].move([x, y - disp*(1.0/6.0), z])
+    cell.setSiteMobilities(False,False,True) # add conditions for this?
+    disp = ('%.3f'%disp).replace('.','')
+    cell.sendToPOSCAR('POSCAR_I_%s'%disp)
+
+halfz = .5*(nlayers-3)/(nlayers+ngap)
+
+# ESF
+for disp in displacements[1:]:    
+    cell = Cell().loadFromPOSCAR('POSCAR_I_1000')
+    for element in cell.sites:
+        for i in range(cell.numberOfElements()):
+            for j in range(cell.numberOfAtomsOfElement(i)):
+                    x,y,z = cell.sites[i][j].position
+                    if z < halfz:
+                        cell.sites[i][j].move([x, y + disp*(1.0/6.0), z])
+    cell.setSiteMobilities(False,False,True) # add conditions for this?
+    disp = ('%.3f'%disp).replace('.','')
+    cell.sendToPOSCAR('POSCAR_E_%s'%disp)
