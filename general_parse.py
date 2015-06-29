db74d94830ef53a7b71953cfc0bc43d18eda516c
jonaskaufman <Jonas@Jonas-MBP-2.materials.unsw.edu.au>
2015-06-29 15:39:07 +1000

    changed to extract E0 not F. also returns lattice parameters

--- general_parse.py
+++ general_parse.py
@@ -12,8 +12,8 @@ import numpy as np
 HOME = '/home1/03022/bassman/Jonas/'
 WORK = '/work/03022/bassman/Jonas/'
 
-def getEnergy(file): # change to parse OSZICAR?
-    """ parses an OUTCAR file and pulls out the free energy of the system """
+def getEnergy(file): # this function returns E(sigma->0), not TOTEN
+    """ parses an OUTCAR file and pulls out the energy of the system """
     f = open(file,'r')
     while True:
         nextLine = f.readline()
@@ -21,8 +21,10 @@ def getEnergy(file): # change to parse OSZICAR?
             break
         if 'FREE ENERGIE OF THE ION-ELECTRON SYSTEM (eV)' in nextLine:
             f.readline()    # line of dashes
+            f.readline()    # TOTEN line
+            f.readline()    # blank line
             energyLine = f.readline().split()
-            energy = float(energyLine[4])
+            energy = float(energyLine[6])
     return energy
     
 def getCellSize(file):
@@ -39,25 +41,28 @@ def getCellSize(file):
             V = float(volumeLine[4])
             for i in range(6):
                 f.readline()    # text
-            a = float(f.readline().split()[1]) # perpendicular lattice vectors
-            # the lattice vector length in loading direction is [0] ## change this?
-            break
-    return (V,a)
+            aLine = f.readline().split()
+            ax = aLine[0]
+            ay = aLine[1]
+            az = aLine[2]
+            aList = [ax,ay,az]
+    return (V,aList)
     
 def parseResults(directory):
     """ finds the energy of each simulation in a given results directory """
     dirList = []
     EList = []
     VList = []
-    for dir in os.listdir(directory): # 0.XXXXX_main or 0.XXXXX_c*_0.XXXXX
+    aLists = []
+    for dir in os.listdir(directory):
         for file in os.listdir(directory + dir):        
                 if fnmatch.fnmatch(file,'OUTCAR'):
                     EList += [getEnergy(directory+dir+'/'+file)]
                     sizeData = getCellSize(directory+dir+'/'+file)
                     VList += [sizeData[0]]
-                    # aList += [sizeData[1]]
+                    aLists += [sizeData[1]]
                     dirList += [dir]     
-    return [dirList,EList,VList] 
+    return [dirList,EList,VList,aLists] 
             
 #===========================================================================
 # MAIN PROGRAM
@@ -68,6 +73,8 @@ data = parseResults(RESULTS+cname+'_results/')
 dirList = data[0]
 EList = data[1]
 VList = data[2]
+aLists = data[3]
 print '\nDirectory names:\n'+str(dirList)
 print '\nVolumes:\n'+str(VList)
+print '\nLattice parameters:\n'+str(aLists)
 print '\nEnergies:\n'+str(EList)
