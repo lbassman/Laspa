9b95a8848da2b4949bffc9cbdeb540c4e1f60157
Jonas Kaufman <jonas.l.kaufman@gmail.com>
2015-07-23 12:35:21 +1000

    changed to do relaxations, final static run

--- stampede_LP.py
+++ stampede_LP.py
@@ -3,16 +3,11 @@
 #==============================================================================
 #  Jonas Kaufman jlkaufman@hmc.edu
 #  February 25, 2015
-#  Script to run VASP calculations necessary to find the lattice parameters
-#  of a fcc, bcc or hcp crystal structure - on Stampede
+#  Script to run VASP calculations necessary to find the ground state
+#  of a given structure - on Stampede
 #==============================================================================
-# This script assumes these lattice vectors (or some multiples) for hcp:
-#  1.00000         0.00000000000000      0.00000
-# -0.50000         0.86602540378444      0.00000
-#  0.00000         0.00000000000000      c/a
-# where a is the overall scaling factor
 """
-Add POSCAR, POTCAR, KPOINTS, and INCAR files to the working directory
+Add POSCAR, POTCAR, KPOINTS, INCAR_static, INCAR_relax, and INCAR_cont
 Make script executable using 'chmod +x _____.py' to call as bash script
 Requires Cell.py
 """
@@ -27,38 +22,61 @@ EMAIL = 'jlkaufman@hmc.edu'
 # Stampede allocation number
 ALLOCATION = 'TG-DMR140093'
 
-def genSubScript(jName,dirList,runLength,nCores,nNodes):
+def genSubScript(reRelax,jName,dirList,runLength,nCores,nNodes):
     """ creates a submission script for Stampede's SLURM queueing system """
-    # uses integer division
+    nDirs = len(dirList)
     hrs = runLength/60
     mins = runLength%60
     string = ('#!/bin/bash\n' +
-    '#SBATCH -J ' + jName +  '\n' +           # specify job name
-    '#SBATCH -o ' + jName + '%j\n' +          # write output to this file
+    '#SBATCH -J %s\n'%(jName) +                 # specify job name
+    '#SBATCH -o '+jName+'_%j\n' +             # write output to this file
     '#SBATCH -n %d\n'%(nCores*len(dirList)) + # request cores
     '#SBATCH -N %d\n'%(nNodes*len(dirList)) + # request nodes
     '#SBATCH -p normal\n' +                   # send to normal queue
     '#SBATCH -t %02d:%02d:00\n'%(hrs,mins) +  # set maximum wall time
-    '#SBATCH --mail-user=' + EMAIL +'\n' +    # set email
+    '#SBATCH --mail-user=%s\n'%(EMAIL) +        # set email
     '#SBATCH --mail-type=all\n' +             # send all emails
-    '#SBATCH -A ' + ALLOCATION + '\n' +       # specify project
+    '#SBATCH -A '+ALLOCATION+'\n'            # specify project
     'module load vasp\n')                     # load vasp module
-    for i in range(len(dirList)):
-        # change to work directory, run vasp
-        string += 'cd '+WORK+'%s\n'%dirList[i]
+    # initial relaxation
+    for i in range(nDirs):
+        string += 'cd %s%s\n'%(WORK,dirList[i])
+        string += 'cp INCAR_relax INCAR\n'
         string += 'ibrun -o %d '%(nCores*i)
         string += '-n %d vasp_std > vasp_output.out &\n'%nCores
-    # wait for all jobs to finish, move to results directory
-    string += 'wait\ncd '+HOME+'\nmkdir %s_results\n'%(jName)
-    for i in range(len(dirList)):
-        # move directories to results directory
-        string += 'cd '+WORK+'\nmv %s '%dirList[i]
-        string += HOME+'%s_results/\n'%jName
+    string += 'wait\n'
+    # re-relaxation, if requested
+    if reRelax:
+        for i in range(nDirs):
+            string += 'cd %s%s\n'%(WORK,dirList[i])
+            string += 'cp INCAR_cont INCAR\n'
+            string += 'cp CONTCAR POSCAR\n'
+            string += 'ibrun -o %d '%(nCores*i)
+            string += '-n %d vasp_std > vasp_output.out &\n'%nCores
+        string += 'wait\n'
+    # copy relaxation results to another directory
+    string += 'cd '+HOME+'\nmkdir %s_relax_results\n'%(jName)
+    for i in range(nDirs):
+        string += 'cd %s\n'%WORK
+        string += 'cp -r %s %s%s_relax_results\n'%(dirList[i],HOME,jName)
+    # final static calculation
+    for i in range(nDirs):
+        string += 'cd %s%s\n'%(WORK,dirList[i])
+        string += 'cp INCAR_static INCAR\n'
+        string += 'cp CONTCAR POSCAR\n'
+        string += 'ibrun -o %d '%(nCores*i)
+        string += '-n %d vasp_std > vasp_output.out &\n'%nCores
+    string += 'wait\n'
+    # move final results to results directory
+    string += 'cd '+HOME+'\nmkdir %s_static_results\n'%(jName)
+    for i in range(nDirs):
+        string += 'cd %s\n'%WORK
+        string += 'mv -r %s %s%s_static_results\n'%(dirList[i],HOME,jName)
     f = open(jName + '_submit','w')
     f.write(string)
     f.close()
 
-def getLatCubic(jName, aList,runLength,nCores,nNodes):
+def getLat(jName, aList, reRelax, runLength,nCores,nNodes):
     """
     creates the necessary POSCARs for a cubic structure
     generates a subdirectory for each vasp run, each with the necessary files,
@@ -73,83 +91,35 @@ def getLatCubic(jName, aList,runLength,nCores,nNodes):
         dirName = '%s_%.5f'%(jName,a)
         dirList += [dirName]
         sp.call(['mkdir',dirName])
-        sp.call('cp POSCAR INCAR KPOINTS POTCAR'.split()+\
+        sp.call('cp POSCAR INCAR_static INCAR_relax KPOINTS POTCAR'.split()+\
                 [dirName])
-        sp.call(('cp -r %s '%dirName)+WORK,shell=True)
+        if reRelax:
+            sp.call(['cp','INCAR_cont',dirName])
+        sp.call('cp -r %s %s'%(dirName,WORK),shell=True)
     # create submission script and run
-    genSubScript(jName,dirList,runLength,nCores,nNodes)
+    genSubScript(reRelax,jName,dirList,runLength,nCores,nNodes)
     sp.call('chmod u+x %s_submit'%jName,shell=True)
-    sp.call(['sbatch','%s_submit'%jName]) 
-
-def getLatHex(jName, aList, caList, runLength, nCores, nNodes):
-    """
-    creates the necessary POSCARs for a hexagonal structure
-    generates a subdirectory for each vasp run, each with the necessary files,
-    moves subdirectories to work directory and runs submission script
-    """
-    dirList = []
-    for a in aList:
-        for ca in caList:
-            cell = Cell().loadFromPOSCAR()
-            cell.setA0(float(a))
-            (x,y,z) = cell.latticeVectors
-            cell.setLatticeVectors([x,y,[0,0,ca]])
-            cell.sendToPOSCAR()    
-            # copy files to subdirectory, move subdirectory to WORK
-            dirName = '%s_%.5f_%.5f'%(jName,a,ca)
-            dirList += [dirName]
-            sp.call(['mkdir',dirName])
-            sp.call('cp POSCAR INCAR KPOINTS POTCAR'.split()+\
-                    [dirName])
-            sp.call(('cp -r %s '%dirName)+WORK,shell=True)
-    # create submission script and run
-    genSubScript(jName,dirList,runLength,nCores,nNodes)  
-    sp.call('chmod u+x %s_submit'%jName,shell=True)
-    sp.call(['sbatch','%s_submit'%jName])   
+    sp.call(['sbatch','%s_submit'%jName])    
 
 #==============================================================================
 #  Main Program
 #==============================================================================
 # get user inputs (with defaults)
-structure = raw_input('Crystal structure (fcc, bcc or hcp): ')
-hcp = False
-if 'h' in structure or 'H' in structure:
-    hcp = True
-    structure = 'hcp'
-elif 'b' in structure or 'B' in structure:
-    structure = 'bcc'
-else:
-    structure = 'fcc'
-print structure,'\n'
-aMin = raw_input('Minimum lattice parameter a in angstroms: ')
+aMin = raw_input('Minimum lattice parameter in angstroms: ')
 if not aMin: aMin = 2.0
 else: aMin = float(aMin)
 print aMin,'\n'
-aMax = raw_input('Maximum lattice parameter a in angstroms: ')
+aMax = raw_input('Maximum lattice parameter in angstroms: ')
 if not aMax: aMax = 4.0
 else: aMax = float(aMax)
 print aMax,'\n'
-aPoints = raw_input('Number of a values: ')
+aPoints = raw_input('Number of values: ')
 if not aPoints: aPoints = 7
 else: aPoints = int(aPoints)
 print aPoints,'\n'
-if hcp:
-    nc = raw_input('Periodicity in c: ')
-    if not nc: nc = 1
-    else: nc = int(nc)
-    print nc,'\n'
-    caMin = raw_input('Minimum c/a ratio: ')
-    if not caMin: caMin = 1.5
-    else: caMin = float(caMin)
-    print caMin,'\n'
-    caMax = raw_input('Maximum c/a ratio: ')
-    if not caMax: caMax = 2.0
-    else: caMax = float(caMax)
-    print caMax,'\n'
-    caPoints = raw_input('Number of c/a values: ')
-    if not caPoints: caPoints = 7
-    else: caPoints = int(caPoints)
-    print caPoints,'\n'  
+reRelax = raw_input('Re-relaxation? (y/n):')
+if 'y' in reRelax or 'Y' in reRelax: reRelax = True
+print reRelax,'\n'
 runLength = raw_input('Maximum run time (minutes): ')
 if not runLength: runLength = 300
 else: runLength = int(runLength)
@@ -173,10 +143,4 @@ print HOME,'\n'
 aList = np.linspace(aMin, aMax, aPoints).tolist()
 print 'a values:'
 print aList,'\n'
-if hcp:
-    caList = np.linspace(nc*caMin, nc*caMax, caPoints).tolist()
-    print 'c/a values:'
-    print caList,'\n'
-    getLatHex(jName,aList,caList,runLength,nCores,nNodes)
-else:
-    getLatCubic(jName,aList,runLength,nCores,nNodes)
+getLat(jName,aList,reRelax,runLength,nCores,nNodes)
